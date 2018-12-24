1858c00c95097eb8d01ec8a5dd18d089dc487e6b
Wu, Hao-Yu <ao6swind@hotmail.com>
2018-12-24 17:49:35 +0800

    簡單的資料庫設計

--- Backend/database/migrations/2018_12_24_072105_create_teachers_table.php
+++ Backend/database/migrations/2018_12_24_072105_create_teachers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateTeachersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email', 100)->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('account');
+            $table->string('password');
+            $table->string('name');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +29,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('teachers');
     }
 }
--- Backend/database/migrations/2018_12_24_074542_create_students_table.php
+++ Backend/database/migrations/2018_12_24_074542_create_students_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateStudentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('students', function (Blueprint $table) {
             $table->increments('id');
             $table->string('account', 100)->unique();
             $table->string('password', 100);
             $table->string('name', 100);
             $table->string('email', 100)->unique();
-            $table->unsignedInteger('department_id');
-            $table->string('roles');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -34,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('students');
     }
 }
