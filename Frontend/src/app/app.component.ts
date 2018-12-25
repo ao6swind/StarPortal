b008fce666017496c2c44150ea2deefe1c164427
Wu, Hao-Yu <ao6swind@hotmail.com>
2018-12-25 14:50:30 +0800

    增加desktop版navbar收展功能

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+  isExplode: boolean = false;
+
+  explodeMenu(event) {
+    this.isExplode = event;
+  }
 }
